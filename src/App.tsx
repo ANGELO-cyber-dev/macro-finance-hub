870b1de2740d1afce2fe6c13158df447bb8e65c9
ANGELO-cyber-dev <angelo@cyber-dev.local>
2026-09-05 01:48:31 +0100

    Update Twelve Data proxy to fetch live XAG/USD spot prices

--- src/App.tsx
+++ src/App.tsx
@@ -128,7 +128,7 @@ const MASTER_INSTRUMENTS: MacroAsset[] = [
 
   // --- COMMODITIES ---
   {
-    symbol: 'XAU/USD',
+    symbol: 'XAU/USD,XAG/USD',
     name: 'Spot Gold / US Dollar',
     class: 'COMMODITIES',
     price: 2516.40,
